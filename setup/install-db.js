d07c40e5d47b494db20844da97a7ab095a993e48
David Irvine <dave@daveirvine.me>
2014-06-01 23:07:59 +0100

    Prompt user during installation process in case they don't want to provide connection details.

--- setup/install-db.js
+++ setup/install-db.js
@@ -1,20 +1,50 @@
 var async = require("async");
+var heredoc = require("heredoc");
 var mysql = require("mysql");
+var prompt = require("prompt");
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "test"
-});
+console.log("Database installation ready to proceed. This requires access to MySQL instance, or you can run the SQL yourself.\n");
 
-async.series([
-	function (callback) { connection.connect(callback); },
-	function (callback) { connection.query("CREATE DATABASE `node_binddb`", callback); },
-	function (callback) { connection.query("CREATE USER 'node_binddb'@'%' IDENTIFIED BY 'test'", callback); },
-	function (callback) { connection.query("GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP ON node_binddb.* TO 'node_binddb'@'%'", callback); },
-	function (callback) { connection.end(callback); }
-], function (err, results) {
-	if (err) {
-		throw err;
+prompt.start();
+
+var remoteSQLProperty = {
+	name: 'useremotesql',
+	message: 'Do you wish to provide credentials for a MySQL instance?',
+	validator: /^[yY][es]*$|^[nN][o]?$/,
+	warning: 'Must respond yes or no',
+	default: 'no'
+};
+
+prompt.get(remoteSQLProperty, function (err, result) {
+	var useRemoteSQL = !!(result.useremotesql.toLowerCase()).match(/[yY]/);
+
+	if (useRemoteSQL) {
+		var connection = mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			password: "test"
+		});
+
+		async.series([
+			function (callback) { connection.connect(callback); },
+			function (callback) { connection.query("CREATE DATABASE `node_binddb`", callback); },
+			function (callback) { connection.query("CREATE USER 'node_binddb'@'%' IDENTIFIED BY 'test'", callback); },
+			function (callback) { connection.query("GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP ON node_binddb.* TO 'node_binddb'@'%'", callback); },
+			function (callback) { connection.end(callback); }
+		], function (err, results) {
+			if (err) {
+				throw err;
+			}
+		});
+	} else {
+		console.log("\nRun the following SQL. Supply a secure password.\n");
+
+		var installSQL = heredoc(function () {/*
+CREATE DATABASE `node_binddb`;
+CREATE USER 'node_binddb'@'%' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP ON node_binddb.* TO 'node_binddb'@'%';
+		*/});
+
+		console.log(installSQL);
 	}
 });
